4a51f15ba27e9cf0723eb606b82a5f06305c7bca
Pablo Daniel Britos <elaqueo@gmail.com>
2021-11-11 18:28:27 -0300

    trying usrs CRUD

--- routes.go
+++ routes.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"myapp/data"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -32,6 +33,41 @@ func (a *application) routes() *chi.Mux {
 			return
 		}
 		fmt.Fprintf(w, "%d: %s", id, u.FirstName)
+	})
+
+	a.App.Routes.Get("/users", func(w http.ResponseWriter, r *http.Request) {
+		users, err := a.Models.Users.GetAll()
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		for _, x := range users {
+			fmt.Fprint(w, x.LastName)
+		}
+	})
+	a.App.Routes.Get("/users/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+		u, err := a.Models.Users.Get(id)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		fmt.Fprintf(w, "%s %s %s", u.FirstName, u.LastName, u.Email)
+	})
+	a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+		u, err := a.Models.Users.Get(id)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		u.LastName = a.App.RandomString(10)
+		err = u.Update(*u)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		fmt.Fprintf(w, "updated %s %s %s", u.FirstName, u.LastName, u.Email)
 	})
 
 	// static routes
